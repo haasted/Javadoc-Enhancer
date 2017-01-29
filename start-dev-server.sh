3b2cd6ef98cf5ba48dceab7ede6240eb2f7e417a
Henrik Aasted Sørensen <has@bitcraft.dk>
2017-01-29 15:41:22 +0100

    Simple bash script for serving the javascript.

---
+++ start-dev-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Javadoc-Enhancer is now available using the bookmarklet that has been copied to the clipboard."
+echo '!function(){function a(a,e){var n=document.createElement("script");n.src=a,n.onload=e,document.getElementsByTagName("head")[0].appendChild(n)}a("//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js",function(){a("//localhost:8000/jdocenhance.js")})}();' | pbcopy
+
+
+cd dist
+python -m SimpleHTTPServer
+cd -
