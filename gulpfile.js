36399d1d4d3505a1851ba0378a5600577bdd3760
Henrik Aasted Sørensen <has@bitcraft.dk>
2017-01-29 15:20:12 +0100

    Add package.json. Strip console.log during minification.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp'),
 	minify = require('gulp-minify'),
-	del = require('del');
-
+	del = require('del'),
+	stripDebug = require('gulp-strip-debug');
 
 gulp.task('default', ['clean', 'minify']);
 
 gulp.task('minify', function() {
   gulp.src('src/jdocenhance.js')
+	.pipe(stripDebug())
     .pipe(minify({}))
     .pipe(gulp.dest('dist'));
 
   gulp.src('src/bookmarklet.js')
+	.pipe(stripDebug())
   	.pipe(minify({}))
   	.pipe(gulp.dest('dist'));
 });
