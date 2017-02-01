f2e8ada353b0a6ab7b19a3e7f53a7053eff640d6
Henrik Aasted Sørensen <has@bitcraft.dk>
2017-02-01 17:57:54 +0100

    Replace shell script with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
 	minify = require('gulp-minify'),
 	del = require('del'),
-	stripDebug = require('gulp-strip-debug');
+	stripDebug = require('gulp-strip-debug'),
+	connect = require('gulp-connect'),
+	ncp = require("copy-paste");
 
 gulp.task('default', ['clean', 'minify']);
 
@@ -15,6 +17,17 @@ gulp.task('minify', function() {
 	.pipe(stripDebug())
   	.pipe(minify({}))
   	.pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', function() {
+	ncp.copy('!function(){function a(a,e){var n=document.createElement("script");n.src=a,n.onload=e,document.getElementsByTagName("head")[0].appendChild(n)}a("//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js",function(){a("//localhost:8080/jdocenhance.js")})}();', function() {
+		console.log('Javadoc-Enhancer is now available using the bookmarklet that has been copied to the clipboard.');
+	});
+
+	connect.server({
+		root: 'src',
+		livereload : false
+	});
 });
 
 gulp.task('clean', function() {
--- start-dev-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Javadoc-Enhancer is now available using the bookmarklet that has been copied to the clipboard."
-echo '!function(){function a(a,e){var n=document.createElement("script");n.src=a,n.onload=e,document.getElementsByTagName("head")[0].appendChild(n)}a("//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js",function(){a("//localhost:8000/jdocenhance.js")})}();' | pbcopy
-
-cd src
-python -m SimpleHTTPServer
-cd -
